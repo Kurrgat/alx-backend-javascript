2befc3c4fcf43ac816dcc51ee356292f2162ca01
Kurrgat <mrcykj@gmail.com>
2024-06-26 18:47:40 +0300

    Task 13: will return an iterator to go through every employee in every department

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,17 +1,8 @@
 export default function createIteratorObject(report) {
-  const allEmployees = Object.values(report.allEmployees).flat();
-  let index = 0;
+  const myarray = [];
+  for (const item of Object.values(report.allEmployees)) {
+    myarray.push(...item);
+  }
 
-  return {
-    next() {
-      if (index < allEmployees.length) {
-        return { value: allEmployees[index++], done: false };
-      } else {
-        return { done: true };
-      }
-    },
-    [Symbol.iterator]() {
-      return this;
-    }
-  };
+  return myarray;
 }
