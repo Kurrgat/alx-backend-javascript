eee74af949b03749069d182b1b171506f86d025f
Kurrgat <mrcykj@gmail.com>
2024-10-10 18:48:52 +0300

    Task 0: Return a Promise using this prototype function getResponseFromAPI()

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        // Simulate an API call with setTimeout
+        setTimeout(() => {
+            // Assuming the API call was successful
+            resolve("Response from API");
+        }, 1000); // Simulating a 1-second delay
+    });
+}
+
+export default getResponseFromAPI;
