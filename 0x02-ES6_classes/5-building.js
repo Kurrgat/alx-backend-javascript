3a73975be8775f3e84204a1a3c423241b3467759
Kurrgat <mrcykj@gmail.com>
2024-10-12 22:31:54 +0300

    Task 6

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,28 @@
+class Building {
+  constructor(sqft) {
+    if (new.target === Building) {
+      throw new TypeError('Cannot construct Building instances directly');
+    }
+    this._sqft = this._validateSqft(sqft);
+  }
+
+  // Validate method
+  _validateSqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Square feet must be a number');
+    }
+    return sqft;
+  }
+
+  // Getter
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Abstract method
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+export default Building;
